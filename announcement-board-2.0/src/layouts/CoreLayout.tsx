69837cbc762f29adce97989eb9127f27e3018679
BdonGordon <bgordon@linamar.com>
2018-03-01 15:29:34 -0500

    Adding all the basic files to get started.

---
+++ announcement-board-2.0/src/layouts/CoreLayout.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class CoreLayout extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4> Annoucement Board </h4>
+            </div>
+        );
+    }
+}
+
+export default CoreLayout;
